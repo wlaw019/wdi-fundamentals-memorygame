8bf24fb8bf0c61bcb3fef33f407827b1f109d294
Wincy Law <wlaw019@gmail.com>
2019-09-12 16:06:31 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,17 +8,36 @@ console.log("User flipped " + cardTwo);*/
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay =[];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
+};
 
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){alert("You found a match!");}
-		else {alert("Sorry, try again.");}
-}
+function flipCard(cardId){
+
+	/*var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped queen");
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped king");*/
+
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	};
+};
+
+flipCard(0);
+flipCard(2);
+
+
 
 
