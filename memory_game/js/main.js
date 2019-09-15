479c7597be6b1678f806c4543d3d95ff8a8d2969
Wincy Law <wlaw019@gmail.com>
2019-09-14 22:19:04 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,13 +32,13 @@ var cardsInPlay =[];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
+  		alert("You found a match!");
 	} else {
-  		console.log("Sorry, try again.");
+  		alert("Sorry, try again.");
 	}
 };
 
-function flipCard(cardId){
+function flipCard(){
 
 	/*var cardOne = cards[0];
 	cardsInPlay.push(cardOne);
@@ -47,6 +47,11 @@ function flipCard(cardId){
 	var cardTwo = cards[2];
 	cardsInPlay.push(cardTwo);
 	console.log("User flipped king");*/
+
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
+
+
 
 	console.log("User flipped "+cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
@@ -58,8 +63,23 @@ function flipCard(cardId){
 	};
 };
 
-flipCard(0);
-flipCard(2);
+
+function createBoard(){
+	for(i=0; i<cards.length; i++){
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
+
+
+
 
 
 
